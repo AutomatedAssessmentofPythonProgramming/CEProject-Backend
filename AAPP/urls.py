a1e642e5390966565be0b65e144f5dc394a054f4
SahatasLee <Sahatasnutlee@gmail.com>
2023-01-25 11:59:33 +0700

    Removing migration folder && startapp authentication custom User model

--- AAPP/urls.py
+++ AAPP/urls.py
@@ -1,19 +1,34 @@
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
 from api.views import TeamViewSet, ExerciseViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register('team', TeamViewSet, basename='Team')
 router.register('exercise', ExerciseViewSet, basename='Exercise')
-# router.register('exercise', ExerciseViewSet, basename='Exercise')
-# router.register(r'product', ProductViewSet, basename='Product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
