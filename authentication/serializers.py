32a823d8d7d68d21412a902f1d86464c4e52f155
SahatasLee <Sahatasnutlee@gmail.com>
2023-01-27 11:28:47 +0700

    complete authentucation, login, reset password, token refresh

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,11 @@
+from django.contrib import auth
+from django.utils.encoding import smart_str, force_str, smart_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+
 from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
+
 from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
@@ -19,3 +26,93 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validate_data):
         return User.objects.create_user(**validate_data)
+    
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+        
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3, write_only=True)
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    username = serializers.CharField(max_length=255, min_length=3, read_only=True)
+    # tokens = serializers.CharField(max_length=68, min_length=6, read_only=True)
+    tokens = serializers.SerializerMethodField()
+    
+    def get_tokens(self, obj):
+        
+        user = User.objects.get(email=obj['email'])
+        
+        return {
+            'access': user.tokens()['access'],
+            'refresh': user.tokens()['refresh']
+        }
+    
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'username', 'tokens']
+    
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        if email and password:
+            print(email, 'password authentication/serializers.py')
+        else:
+            msg = 'Both "username" and "password" are required.'
+            raise serializers.ValidationError(msg, code='authorization')
+        
+        user = auth.authenticate(request=self.context.get('request'), email=email, password=password)
+        # user = auth.authenticate(email=email, password=password)
+        if user:
+            print('user:', user, 'username:', user.username)
+        else:
+            print('user:', user)
+            raise AuthenticationFailed('Authen Failed')
+        
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        
+        if not user.is_active:
+            raise AuthenticationFailed('Account disabled, contact admin.')
+        
+        return {
+            'email': user.email,
+            'username': user.username,
+            'tokens': user.tokens()
+        }
+        
+class ResetPasswordEmailRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(min_length=2)
+    
+    class Meta:
+        fields = ['email']
+
+class SetNewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(min_length=6, max_length=68, write_only=True)
+    token = serializers.CharField(min_length=1, write_only=True)
+    uidb64 = serializers.CharField(min_length=1, write_only=True)
+    
+    class Meta:
+        fields = ['password', 'token', 'uidb64']
+        
+    def validate(self, attrs):
+        
+        try:
+            password = attrs.get('password')
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+            
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+            
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise AuthenticationFailed('The reset link is invalid(PasswordResetToken)', 401)
+            
+            user.set_password(password)
+            user.save()
+            
+            return (user)
+        except Exception as e:
+            raise AuthenticationFailed('The reset link is invalid(Expection) + '+ str(e), 401)
